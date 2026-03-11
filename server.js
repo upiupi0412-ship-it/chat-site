c6bd3fbacd0e418b3a6a8c764931243e86bf3fad
UpiUpiUpi <upiupi0412@gmail.com>
2026-03-11 15:55:44 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const http = require("http")
+const { Server } = require("socket.io")
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.use(express.static("public"))
+
+const rooms = {}
+
+io.on("connection", (socket) => {
+
+    socket.on("joinRoom", ({roomName, userName}) => {
+
+        if (!rooms[roomName]) {
+            rooms[roomName] = { users: [] }
+        }
+
+        const room = rooms[roomName]
+
+        room.users.push(userName)
+
+        socket.join(roomName)
+        socket.roomName = roomName
+        socket.userName = userName
+
+        io.to(roomName).emit("userList", room.users)
+    })
+
+    socket.on("disconnect", () => {
+
+        const roomName = socket.roomName
+        if (!roomName) return
+
+        const room = rooms[roomName]
+        if (!room) return
+
+        room.users = room.users.filter(u => u !== socket.userName)
+
+        io.to(roomName).emit("userList", room.users)
+    })
+
+})
+
+server.listen(3000)
